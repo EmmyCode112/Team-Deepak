5ccac329d571d4494029b8d8b5567567743ffbfd
EmmyCode112 <emmywiser3@gmail.com>
2024-07-04 10:58:46 +0100

    Prepare for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.render(
+  <HashRouter>
     <App />
-  </React.StrictMode>,
-)
+  </HashRouter>,
+  document.getElementById("root")
+);
